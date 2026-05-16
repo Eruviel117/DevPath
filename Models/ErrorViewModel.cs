b5c9b5ba0da736d2e3ab999359052990a0bdec1c
Euruviel <euruviel.marquez@tecdesoftware.edu.mx>
2026-05-15 20:52:32 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevPath.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
