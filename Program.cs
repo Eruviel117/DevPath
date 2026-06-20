979aa127226eb175f7e9a50e091fa79784159f53
Euruviel <euruviel.marquez@tecdesoftware.edu.mx>
2026-06-20 15:35:10 -0600

    feat: configurar Swagger para documentar la API REST

--- Program.cs
+++ Program.cs
@@ -8,14 +8,34 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DevPathContext>(options =>
     options.UseSqlServer(builder.Configuration
         .GetConnectionString("DevPathContext")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "DevPath API",
+        Version = "v1",
+        Description = "API REST para gestionar Áreas y Habilidades del sistema DevPath."
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "DevPath API v1");
+    });
 }
 
 app.UseHttpsRedirection();
@@ -28,6 +48,5 @@ app.MapStaticAssets();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 app.Run();
